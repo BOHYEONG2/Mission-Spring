8d3440607b3b0c2d014a84fb9b14c615428184f4
김보형 <kerong12@naver.com>
2023-07-25 15:06:35 +0900

    no message

--- src/main/java/kr/co/mlec/reply/controller/ReplyController.java
+++ src/main/java/kr/co/mlec/reply/controller/ReplyController.java
@@ -11,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import kr.co.mlec.reply.service.ReplyService;
 import kr.co.mlec.reply.vo.ReplyVO;
 
-@RestController // 모든 uri통신을 비동기 통신하려고 할 때 RestController를 사용 RestController이기떄문에 리턴타입은 
+@RestController // 모든 uri통신을 비동기 통신하려고 할 때 RestController를 사용 RestController이기떄문에 리턴타입은 단순문자열 or json객체
 public class ReplyController {
+	
 
 	@Autowired
 	private ReplyService replyService;
